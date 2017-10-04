789aab698573ca7365b2632ca1620b785d090d4b
Louis Vanhove <louis.vanhove2017@campus-eni.fr>
2017-10-04 10:14:51 +0200

    Modifications Utils et ajout OrderDao

--- src/fr/lala/expeditor/utils/PDFUtils.java
+++ src/fr/lala/expeditor/utils/PDFUtils.java
@@ -33,7 +33,7 @@ public class PDFUtils {
         
         setSenderBlock(canvas);
         setReceiverBlock(order.getCustomer(), canvas);
-        setOrderInfo(order.getId(), canvas);
+        setOrderInfo(order, canvas);
         setOrderDetail(order, canvas);
 
         document.close();
@@ -73,12 +73,12 @@ public class PDFUtils {
      * @param numFacture
      * @param canvas
      */
-    private static void setOrderInfo(int numFacture, PdfContentByte canvas) {
-    	ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, new Phrase("Commande n° "+numFacture), 50, 670, 0);	
+    private static void setOrderInfo(Order order, PdfContentByte canvas) {
+    	ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, new Phrase("Commande n° "+order.getId()), 50, 670, 0);	
     	
     	BarcodeEAN codeEAN = new BarcodeEAN();
     	codeEAN.setCodeType(Barcode.EAN13);
-    	codeEAN.setCode("9780201615883");
+    	codeEAN.setCode(sixDigitsOrderNumber(order));
     	Image imageEAN = codeEAN.createImageWithBarcode(canvas, null, null);
     	imageEAN.setWidthPercentage(5);
     	imageEAN.setAbsolutePosition(350, 670);
@@ -97,7 +97,9 @@ public class PDFUtils {
 			}
 	}
     
-    /**
+
+
+	/**
      * Permet d'afficher la liste des articles dans le tableau
      * @param data
      * @param canvas
@@ -122,5 +124,41 @@ public class PDFUtils {
 		}
         
     }
+    
+    /**
+     * Méthode en charger de transformer l'id de la commande 
+     * en cnuméro de commande suffisamment long pour le code barre
+     * 
+     * @param order
+     * @return
+     */
+    private static String sixDigitsOrderNumber(Order order) {
+		String result = "1" ;
+    	int zeroToAdd = 0 ;
+		int id = order.getId();
+		
+		if (id < 10) 
+			zeroToAdd = 4 ;
+		else if (id < 100) 
+			zeroToAdd = 3 ;
+		else if (id < 1000) 
+			zeroToAdd = 2 ;
+		else if (id < 10000) 
+			zeroToAdd = 1 ;
+		else if (id < 10000) 
+			zeroToAdd = 0 ;
+		else
+			result = String.valueOf(id);
+	
+		if(zeroToAdd > 0){
+			for (int i = 0 ;  i < zeroToAdd; i++){
+				result += "0";
+			}
+		}
+		
+		return result;
+		
+
+	}
 
 }
